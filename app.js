9ec4e9e7c7c5f9c34b0fa7b5ee6b5ae36cce36c0
Nerea <neresaas@gmail.com>
2024-03-03 20:52:47 +0100

    middleware for /items and /bids

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 let express = require('express');
+let jwt = require('jsonwebtoken');
 let app = express();
 let port = 8081;
 
 app.use(express.json());
+
+app.use(['/items', '/bids'], (req, res, next) => {
+    console.log('middleware')
+
+    let apiKey = req.query.apiKey
+
+    if (apiKey == undefined) {
+        res.status(401).json({error: 'not apiKey'})
+        return
+    }
+
+    let infoApiKey
+    try{
+    let infoApiKey = jwt.verify(apiKey, 'secret')
+    } catch (error) {
+        res.status(401).json({error: 'not valid apiKey'})
+        return
+    }
+
+    req.infoApiKey = infoApiKey
+
+    next()
+});
 
 let routerItems = require('./routers/routerItems');
 let routerBids = require('./routers/routerBids');
